af8633e71a848cd95b62c73cf62da7d8dd401a8f
AsifHasanOntu <150927981+AsifHasanOntu@users.noreply.github.com>
2023-11-16 16:37:54 +0600

    Update Untitled1.c

--- Untitled1.c
+++ Untitled1.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
 
 int main() {
-  printf("Hello World!");
-  return 0;
+    int numSubjects;
+
+    // Take the number of subjects as input
+    printf("Enter the number of subjects: ");
+    scanf("%d", &numSubjects);
+
+    // Declare an array to store GPAs
+    float gpaArray[numSubjects];
+
+    // Take GPA input for each course
+    for (int i = 0; i < numSubjects; ++i) {
+        printf("Enter GPA for Subject %d: ", i + 1);
+        scanf("%f", &gpaArray[i]);
+    }
+
+    // Calculate average GPA
+    float sum = 0;
+    for (int i = 0; i < numSubjects; ++i) {
+        sum += gpaArray[i];
+    }
+    float average = sum / numSubjects;
+
+    // Print the average GPA
+    printf("\nAverage GPA: %.2f\n", average);
+
+    // Print GPA for each course
+    printf("\nIndividual GPAs:\n");
+    for (int i = 0; i < numSubjects; ++i) {
+        printf("Subject %d: %.2f\n", i + 1, gpaArray[i]);
+    }
+
+    return 0;
 }
